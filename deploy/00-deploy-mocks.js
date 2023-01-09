8e16220e9b591b7f04e2fcdc32ed6aa6a41a6c48
Olaoye Salem <89555234+olaoyesalem@users.noreply.github.com>
2023-01-09 10:56:04 -0800

    Update 00-deploy-mocks.js

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,7 +1,7 @@
 const { network } = require("hardhat");
 const { developmentChains,DECIMALS,INITIAL_ANSWER} = require("../helper-hardhat-config");
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+module.exports = async ({ getNamedAccounts, deployments}) => {
 	const { log, deploy } = deployments;
 	const { deployer } = await getNamedAccounts();
 	const chainId = network.config.chainId;
